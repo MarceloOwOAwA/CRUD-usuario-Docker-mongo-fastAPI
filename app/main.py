5aea9cf47b39a6d2af8b703269b11ec938278cc0
Marcelo <marceloordenesalbornoz@gmail.com>
2025-08-12 12:08:40 -0400

    Se agrega red y permisos para que webCRUD interacture con este componente

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import registro
 from app.routes import login
 from app.routes import deleteuser
@@ -11,3 +12,11 @@ app.include_router(registro.router, prefix="/user", tags=["Registro"])
 app.include_router(login.router, prefix="/user", tags=["login"])
 
 app.include_router(deleteuser.router, prefix="/user", tags=["delete_user"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],   # or ["*"] for any origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
